37204dfc0953998c171c16340fb0464541e30690
Andrew Horge <theremlyfe@gmail.com>
2015-08-18 12:25:29 -0400

    modified js functions

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Etch-A-Sketch
+# Etch-A-Sketch Project
+
+Task:
+
+To create a digital version of the popular toy Etch A Sketch
+
+Steps:
+
+Get input from user
+Use input to determine the "screen" resolution / grid size
+Have the background color of each section of the grid change as the user's mouse hovers above it
+Have the background color of each section of the grid change to a random color as the user's mouse hovers above it
+Have the background color of each section of the grid change as the user's mouse hovers above it then have the color lighter or darken with each successive hover
+**(In Progress)Have the background color of each section of the grid change to a random color And play an individual note from a song as the user's mouse hovers above it
+
+
+
+Resources used:
+Google
+Stack Overflow
+Paul Irish blog post //http://www.paulirish.com/2009/random-hex-color-code-snippets/
+CSS Tricks
+jQuery Documentation
+W3Schools
+Git - The Simple Guide http://rogerdudler.github.io/git-guide/
+
+
--- etch.js
+++ etch.js
@@ -1,33 +1,44 @@
 $(document).ready(function(){ 
 	//Create the default grid using jquery
+		
+		//Do / While loop gets grid size from user with data error checking
 		do {
 				var boxesPerRow = prompt("Enter a number between 1-64");
 			}
 			while (boxesPerRow < 1 || boxesPerRow > 64 || isNaN(boxesPerRow));
 	
 
+	//Declare variables and functions
 
-	var containerWidth = 800; //sets the width of the 'screen' should math the css setting
+	var containerWidth = 800; //sets the width of the 'screen' should match the css setting
 	var boxHeightWidth = Math.round(containerWidth / boxesPerRow); //divides the container by the amount of boxes chosen to create the squares' height and width values
+	
 	var classic = function(){
 		$(".square").mouseenter(function(){
 				$(this).css({"background-color": "black"}); //'this' allows each individual square div's background to change as the mouse enters the div
 			});
 	};
 
-	var opacityChangeOnHover = function() {
-    		$( ".square" ).mouseenter(function() {
-        		var actualOpacity = $( this ).css( "opacity" );
-        		var newOpacity = actualOpacity + 0.1;
+	var opacityDiv = function(){
+		$(".square").mouseenter(function(){
 
-        	$( this ).css( {"opacity" : newOpacity} );
-    	});
+			var currentOpacity = $(this).css("opacity");
+
+			if (currentOpacity > 0){
+				$(this).css("opacity", currentOpacity - 0.1);
+			}
+		});
+	};
+
+	var randomColorDiv = function() {
+    		$(".square").mouseenter(function(){
+			$(this).css({"border-radius" : "50px" , "background-color" : "#" + Math.floor(Math.random() * 16777215).toString(16)}); //http://www.paulirish.com/2009/random-hex-color-code-snippets/
+		});
 	};
 
 	var clearGrid = function(){
 		$("#container > .square").remove();
 		boxesPerRow;
-		//createGrid();
 	}
 
 	var createGrid = function(){
@@ -40,14 +51,15 @@ $(document).ready(function(){
 
 			createGrid();
 
+//Set up button functionality
+
 $("#colors").click(function(){
 		clearGrid();
 		createGrid();
+		randomColorDiv();
 
-		$(".square").mouseenter(function(){
-			$(this).css({"border-radius" : "50px" , "background-color" : "#" + Math.floor(Math.random() * 16777215).toString(16)}); //http://www.paulirish.com/2009/random-hex-color-code-snippets/
-		});
 	});
+
 $("#classic").click(function(){
 	clearGrid();
 	createGrid();
@@ -60,17 +72,7 @@ $("#clear").click(function(){
 $("#shades").click(function(){
 	clearGrid();
 	createGrid();
-
-	$(".square").mouseenter(function(){
-
-		var currentOpacity = $(this).css("opacity");
-
-		if (currentOpacity > 0){
-			$(this).css("opacity", currentOpacity - 0.1);
-		}
-	});
-
-		
+	opacityDiv();
 
 	});
 		
